cab5f786d1de432b598407397c95362bbbfc13d4
Alessandro1312 <alessandro.martucci2007@gmail.com>
2024-05-07 13:24:10 +0000

    versione 2

--- ex12.c
+++ ex12.c
@@ -6,15 +6,15 @@ int main()
     printf("anno di nascita: ");
     scanf("%d", &anno);
 
-    if(anno<1969)
+    if(anno<=1969)
     {
         printf("sei nato prima dell'arrivo dell'uomo sulla luna\n");
-        printf("1969-anno, %d anni prima\n", 1969-anno);
+        printf("%d anni prima\n", 1969-anno);
 
     }
     else
     {
         printf("sei nato dopo l'atterraggio sulla luna\n");
-        printf("anno-1969: %d anni dopo\n", anno-1969);
+        printf("%d anni dopo\n", anno-1969);    
     }
 }
