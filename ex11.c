510a2598583e048ea219d5e45c25f2f0be889f5c
Alessandro1312 <alessandro.martucci2007@gmail.com>
2024-05-07 12:36:38 +0000

    versione g

---
+++ ex11.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a, b, c;
+    printf("lunghezza di a:");
+    scanf("%d", &a);
+    printf("lunghezza di b:");
+    scanf("%d", &b);
+    printf("lunghezza di c:");
+    scanf("%d", &c);
+
+    if((a+b)>=c && (a+c)>=b && (b+c)>=a)
+    {
+        printf("è un triangolo\n");
+       
+        if(a==b && b==c)
+        {
+            printf("è un triangolo equilatero\n");
+        }
+        else if(a==b || b==c || c==a)
+        {
+            printf("è un triangolo isoscele\n");
+        }
+
+        else
+        {
+            printf("è un triangolo scaleno\n");
+        }
+    }
+    else
+    {
+        printf("non è un triangolo\n");
+    }
+
+    
+}
