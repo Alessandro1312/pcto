af365f5d5bf73799858553485b3375c9006815d5
Alessandro1312 <alessandro.martucci2007@gmail.com>
2024-05-07 10:41:29 +0000

    versione d

---
+++ ex09.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int eta;   
+    printf("scrivi la tua eta:");
+    scanf("%d", &eta);
+ 
+
+    //OPERATORI LOGICI: && and, || or, !not//
+    if((eta>0 && eta<30)||(eta>60 && eta<100))
+    {
+        printf("entri\n");
+    
+    }
+    else
+    {
+        printf("non entri\n");
+    }
+}
