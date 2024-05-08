067463e4497aa3af127419d51f352528efd860d9
Alessandro1312 <alessandro.martucci2007@gmail.com>
2024-05-08 17:20:16 +0000

    versione 1 gioco

--- gioco.c
+++ gioco.c
@@ -2,5 +2,20 @@
 
 int main()
 {
-    int nome[3]=
+    int a;
+    printf("scegli un personaggio\n1. Apollo\n2. Demetra\n3. Zeus\n");
+    scanf("%d", &a);
+
+    if(a==1)
+    {
+        printf("hai scelto Apollo\n");
+    }
+    else if (a==2)
+    {
+        printf("hai scelto Demetra\n");
+    }
+    else if (a==3)
+    {
+        printf("hai scelto Zeus\n");
+    }
 }
