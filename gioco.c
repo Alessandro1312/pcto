bc2d1da540016c54f4b95e9a87b0ef75dd45e2ed
Alessandro1312 <alessandro.martucci2007@gmail.com>
2024-05-08 17:06:55 +0000

    versione 7

--- gioco.c
+++ gioco.c
@@ -3,20 +3,4 @@
 int main()
 {
     int nome[3]=
-
-    printf("scegli il personaggio 1/2/3\n");
-    scanf("%c", &Apollo),
-    
-    if(1)
-    {
-        printf("1.Hai scelto Apollo\n");
-    }
-    else if(2)
-    {
-        printf("2.Hai scelto Demetra\n");
-    }
-    else
-    {
-        printf("3.Hai scelto Zeus\n");
-    }
 }
