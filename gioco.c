63e8dc991eb2720ad782ecf4341778ef5bc42523
Alessandro1312 <alessandro.martucci2007@gmail.com>
2024-05-09 07:25:02 +0000

    versione 8 gioco

--- gioco.c
+++ gioco.c
@@ -3,7 +3,7 @@
 int main()
 {
     int a;
-    printf("ciao benvenuto in questo gioco\nscegli un personaggio\n1. Apollo\n2. Demetra\n3. Zeus\n");
+    printf("ciao benvenuto in questo gioco\nscegli un personaggio\n1. Apollo\n2. Demetra\n3. Zeus\n4. Afrodite\n");
     scanf("%d", &a);
 
     if(a==1)
@@ -47,9 +47,40 @@ int main()
     }
     else if (a==3)
     {
+        int d;
         printf("hai scelto Zeus\n");
         printf("re degli dei dio del cielo e governatore dei fenomeni meteorologici il suo simbolo e il tuono scatena una guerra contro suo padre Crono per salvare i suoi fratelli inghiottiti dallo stesso genitore\n ");
-
+        printf("In alto sui monti nella dimora degli dei Siede Zeus sovrano dei cieli e dei re due figli devoti dal cuore sincero Chiesero al padre un destino vero Uno desiderava la forza immensa l altro la saggezza\nla mente intensa Zeus sorrise ma una sola via poteva scegliere Per ognuno un dono da non\nrimpiangere Al primo donò la forza del tuono All'altro la saggezza\ncome un antico libro in ozio E così il destino fu sigillato con amore e con cura.\n Quale figlio ricevette la sua parte più pura?\n");
+        printf("1. il figlio che ricevette la forza del tuono\n");
+        printf("2. il figlio che ricevette la saggezza\n");
+        scanf("%d", &d);
         
+        if(d==1)
+        {
+            printf("accidenti hai fatto la scelta sbagliata ricordati sempre che la saggezza batte la forza\n");
+        }
+
+        else if(d==2)
+        {
+            printf("complimenti hai scelto la risposta giusta la saggezza vince sempre contro la forza\n");
+        }
+
     }
+    
+    else if(a==4)
+    {
+        printf("Hai scelto Afrodite\n");
+        printf("Afrodite la dea greca dell amore della bellezza e della fertilità emerge dalla schiuma marina nata dalla\nspuma che si formo intorno ai genitali di Urano tagliati dal dio Crono splendida e desiderabile e rappresentata con grazia e sensualita circondata da mirti e colombi\nIl suo amore incanta sia gli dei che gli uomini e il suo potere influisce sulle passioni più profonde\nprotettrice degli amanti e delle unioni incarna la forza irresistibile dell attrazione e la bellezza che risiede nell armonia\n");
+        printf("Afrodite compie questa scelta cruciale nel momento in cui decide di influenzare il destino amoroso di due amanti. ");
+        printf("");
+    
+    }
+    
+    else
+    {
+        printf("Perchè non rispetti le regole. Adesso ti tocca riniziare.\n");
+    }
+
+    
+    printf("Grazie per aver provato questo gioco ora ti sfido con un altro personaggio.\n");
 }
